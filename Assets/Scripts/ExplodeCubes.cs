5e33d82823cb923aadd461ed213996d135d875cf
Daulet <26594@iitu.edu.kz>
2021-10-24 05:51:14 +0600

    create some script file & add new materials, sprite for UI

--- Assets/Scripts/ExplodeCubes.cs
+++ Assets/Scripts/ExplodeCubes.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ExplodeCubes : MonoBehaviour
 {
+    public GameObject restartButton;
     private bool _collisionSet;
     private void OnCollisionEnter(Collision collision)
     {
@@ -17,6 +18,8 @@ public class ExplodeCubes : MonoBehaviour
                 child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(70f, Vector3.up,5f);
                 child.SetParent(null);
             }
+            restartButton.SetActive(true);
+            Camera.main.transform.position -= new Vector3(0,0,3f);
             Destroy(collision.gameObject);
             _collisionSet = true;
         }
